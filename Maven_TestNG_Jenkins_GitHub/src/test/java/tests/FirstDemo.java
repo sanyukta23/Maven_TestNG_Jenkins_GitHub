2997b42312c49cb04943cc2bfe6243449e3f5cf1
DELL <DELL@DELL-PC>
2018-02-11 13:13:27 +0530

    <apande> initial commit

---
+++ Maven_TestNG_Jenkins_GitHub/src/test/java/tests/FirstDemo.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstDemo {
+  @Test
+  public void f() 
+  {
+	  System.out.println("Hi");
+	  System.out.println("Hi");
+	  System.out.println("Hi");
+	  
+  }
+}
